e40bf279c596a86e6e9e4127365a4231812fdce7
parthshukla <parthshukla808@gmail.com>
2024-10-25 18:30:07 +0530

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState ,useContext} from 'react'
+
+import UserContext from '../context/UserContext';
+
+function Profile() {
+    const {user} = useContext(UserContext);
+    if(!user) return <h1>please login</h1>
+  return (
+    <div>
+        <h1>
+        Welcome {user.username}
+        </h1>
+      
+    </div>
+  )
+}
+
+export default Profile
---
+++ src/context/UserContext.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const UserContext =React.createContext(); //like a grobal variable , and makeing a provide to access the varivale
+
+
+export default UserContext;
